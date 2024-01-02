bba9998eb893c033a8716ef5e91eaf3d1060f667
mberrouk <mberrouk@student.1337.ma>
2024-01-02 21:08:33 +0000

    d07 complete

--- d07/ex02/Array.hpp
+++ d07/ex02/Array.hpp
@@ -2,6 +2,7 @@
 #define ARRAY_HPP
 
 #include <cstddef>
+#include <iostream>
 #include <stdexcept>
 template<typename T>
 class Array {
@@ -15,26 +16,27 @@ class Array {
 			if (this != &src) {
 				if (this->_arr)
 					delete[] _arr;
-				this->size = src.size();
-				this->_arr = new T[this->size];
-				for (size_t i = 0; i < this->size; i++)
+				this->_size = src.size();
+				this->_arr = new T[this->_size];
+				for (size_t i = 0; i < this->_size; i++)
 					this->_arr[i] = src._arr[i];
 			}
-
+			return (*this);
 		}
-		T &operator[](size_t index) {
-			if (index >= this->size()) {
-				std::runtime_error("Index Is Out Of Bounds.");
+		T &operator[](int index) {
+			// std::cout << "in operator " << index << std::endl;
+			if (index >= this->size() || index < 0) {
+				throw std::runtime_error("Index Is Out Of Bounds.");
 			}
 			return (_arr[index]);
 		}
 		~Array() { delete[] _arr; }
-		size_t size() { return (this->size);}
+		int size() const { return (this->_size);}
 
 
 	private:
 		T *_arr;
-		size_t _size;
+		int _size;
 };
 
 #endif
