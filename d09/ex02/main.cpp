3ae6464a548d7ca5877f650cbe15c1536b912689
mberrouk <mberrouk@student.1337.ma>
2024-01-15 10:34:45 +0100

    init

--- d09/ex02/main.cpp
+++ d09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mberrouk <mberrouk@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 07:21:37 by mberrouk          #+#    #+#             */
-/*   Updated: 2024/01/15 09:58:21 by mberrouk         ###   ########.fr       */
+/*   Updated: 2024/01/15 10:34:23 by mberrouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@
 int main(int argc, const char *argv[]) {
     
     t_input input;
+    std::vector<int> vec;
     int n;
 
     if (argc < 2)
         return 1;
-
+   if (init_data(input.seqInt, argv, argc))
+        return 2;
+        
     input.clockS = std::clock();
-    if (init_data(input.seqInt, argv, argc))
+    if (init_data(vec, argv, argc))
         return 2;  
-    Ford_JohnsonSort(input.seqInt);
+    Ford_JohnsonSort(vec);
     input.clockE = std::clock();
 
 
@@ -39,15 +42,19 @@ int main(int argc, const char *argv[]) {
     std::cout << "Before: ";
     print_array(input.seqInt);
     std::cout << "After: ";
-    print_array(input.seqInt);
+    print_array(vec);
     
-    long double time = static_cast<long double>((input.clockE - input.clockS)* 1000.0) / CLOCKS_PER_SEC ;
-    std::cout << "Time to process a range of "<< input.seqInt.size() 
+    long double time =
+        static_cast<long double>((input.clockE - input.clockS)*1000.0) / CLOCKS_PER_SEC ;
+    
+    std::cout << "Time to process a range of "<< vec.size() 
         << " elements with std::vector : " << time << std::endl;
     
-    time = static_cast<long double>((deque_endtime - deque_start)* 1000.0) / CLOCKS_PER_SEC;
-    std::cout << "Time to process a range of "<< seq.size() << " elements with std::deque : " << \
-        time << std::endl;
+    time =
+        static_cast<long double>((deque_endtime - deque_start)*1000.0) / CLOCKS_PER_SEC;
+    
+    std::cout << "Time to process a range of "<< seq.size() 
+        << " elements with std::deque : " << time << std::endl;
 
     return (0);
 
